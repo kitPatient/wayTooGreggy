9eb78f655bef9035c9c02f5de231806815fbec75
kitPatient <kitpatient28@gmail.com>
2022-05-10 23:52:43 +0100

    refactor

--- discordBot.py
+++ discordBot.py
@@ -5,25 +5,10 @@ import random
 
 debugMode = False
 
-def getBotToken():
-	if(debugMode):
-		print("Loading Token!")
-	x = open("botToken.token","r")
-	xr = x.read()
-	x.close()
-	return xr
-
-botToken = getBotToken()
-
-if(debugMode):
-	print(botToken)
-
-#Loads Commands as a json to commandHelp
-commandHelpL = open("command_helper.json", "r")
-commandHelpg = commandHelpL.read()
-commandHelp = json.loads(commandHelpg)
-commandHelpL.close()
-
+'''---------COMMON VARIABLES AND LINKS---------'''
+#What displays the bot as currently playing
+currentlyPlaying = "Not Too Greggy"
+ 
 #Link to Github Repo
 linkREPO = "https://github.com/kitPatient/GregsJourney"
 
@@ -40,51 +25,123 @@ emojiA = '🇦'
 emojiB = '🇧'
 
 #load the client
-client = discord.Client(activity=discord.Game(name='Not Too Greggy'))
+client = discord.Client(activity=discord.Game(name=currentlyPlaying))
 
-def find_between( s, first, last ):
+'''---------COMMON FUNCTIONS---------'''
+
+def getBotToken():
+	if(debugMode):
+		print("Loading Token!")
+	x = open("botToken.token","r")
+	xr = x.read()
+	x.close()
+	return xr
+
+def loadJsonData(filename):
+    _a = open(filename,"r")
+    _ar = _a.read()
+    _aj = json.loads(_ar)
+    _a.close()
+    return _aj
+
+def dumpJsonData(filename, data, indent = 2):
+    _a = open(filename,"w")
+    _as = json.dumps(data,indent = indent)
+    _a.write(_as)
+    _a.close()
+    
+    #jsonFile = open("json_poll.json", "w")
+	#jsonString = json.dumps(dict,indent=2)
+	#jsonFile.write(jsonString)
+	#jsonFile.close()
+
+#Same as dumpJsonData
+#def updateJson(filename,data,indent=2):
+    #jsonFile = open(filename,"w")
+    #jsonString = json.dumps(data,indent=indent)
+    #jsonFile.write(jsonString)
+    #jsonFile.close()
+    
+def find_between(s, first, last):
     try:
-        start = s.index( first ) + len( first )
-        end = s.index( last, start )
+        start = s.index(first) + len(first)
+        end = s.index(last,start)
         return s[start:end]
     except ValueError:
         return ""
 
-def saveDict(dict):
-	jsonFile = open("json_poll.json", "w")
-	jsonString = json.dumps(dict,indent=2)
-	jsonFile.write(jsonString)
-	jsonFile.close()
+#def saveDict(dict):
+    #dumpJsonData("json-poll.json",dict)
+    #jsonFile = open("json_poll.json", "w")
+    #jsonString = json.dumps(dict,indent=2)
+    #jsonFile.write(jsonString)
+    #jsonFile.close()
 
-def updateVersion(newVersion):
-	jsonFile = open("version.json", "w")
-	x = {"version": newVersion}
-	print(x)
-	jsonString = json.dumps(x,indent=2)
-	print(jsonString)
-	jsonFile.write(jsonString)
+#def updateVersion(newVersion):
+	#jsonFile = open("version.json", "w")
+	#x = {"version": newVersion}
+	#print(x)
+	#jsonString = json.dumps(x,indent=2)
 	#print(jsonString)
-	jsonFile.close()
-
+	#jsonFile.write(jsonString)
+	#print(jsonString)
+	#jsonFile.close()
+ 
 async def addABReactions(message):
-	await message.add_reaction(emojiA)
-	await message.add_reaction(emojiB)
+    await message.add_reaction(emojiA)
+    await message.add_reaction(emojiB)
 
 async def Mesage(message, response):
-	return await message.channel.send(response)
+    return await message.channel.send(response)
 
 async def EmbedMessage(message,response):
-	return await message.channel.send(embed=response)
+    return await message.channel.send(embed=response)
 
-async def EmbededWthPic(message, urll, colorx=0xFF5733):
-	x = discord.Embed(color=colorx)
-	x.set_image(url=urll)
-	return await EmbedMessage(message,x)
+async def EmbededWthPic(message, url, colorx=0xFF5733):
+    x = discord.Embed(color=colorx)
+    x.set_image(url=url)
+    return await EmbedMessage(message,x)
+
+def loadHelpEmbed():
+    NcommandHelp = loadJsonData("command_helper.json")
+    NCcommandHelp = NcommandHelp["commands"]
+    x = discord.Embed(title="Command Display",color=0xFF5733)
+    x.set_thumbnail(url=linkGREGTECHLOGO)
+    x.add_field(name="$hello", value=NCcommandHelp["hello"], inline=False)
+    x.add_field(name="$gtnh", value=NCcommandHelp["gtnh"], inline=False)
+    x.add_field(name="$poll", value=NCcommandHelp["poll"], inline=False)
+    x.add_field(name="$link", value=NCcommandHelp["link"], inline=False)
+    x.add_field(name="$newPoll", value=NCcommandHelp["newPoll"], inline=False)
+    x.add_field(name="$flip", value=NCcommandHelp["flip"], inline=False)
+    x.add_field(name="$status", value=NCcommandHelp["status"], inline=False)
+    x.add_field(name="$setStatus", value=NCcommandHelp["setStatus"],inline=False)
+    x.add_field(name="$help", value=NCcommandHelp["help"], inline=False)
+    #mes = await message.channel.send(embed = x)
+    return x
+
+'''---------START OF BOT---------'''
+
+botToken = getBotToken()
+
+if(debugMode):
+	print(botToken)
+
+#Loads Commands as a json to commandHelp
+#commandHelpL = open("command_helper.json", "r")
+#commandHelpg = commandHelpL.read()
+#commandHelp = json.loads(commandHelpg)
+#commandHelpL.close()
+
+#commandHelp = loadJsonData("command_helper.json")
 
 @client.event
 async def on_message_edit(before, after):
-	print('Old Message by: {0.author.display_name}, With Message: {0.content}'.format(before))
-	print('New Message: {0.content}'.format(after))
+    
+    if (before.author or after.author == client.user):
+        return
+    else:
+        print('Old Message by: {0.author.display_name}, With Message: {0.content}'.format(before))
+        print('New Message: {0.content}'.format(after))
 
 @client.event
 async def on_ready():
@@ -104,24 +161,36 @@ async def on_message(message):
     if message.content.startswith('$epoll1'):
         not0the0flash = await client.fetch_user(IDnot0the0flash)
         if message.author == not0the0flash:
-            with open('json_poll.json') as json_file:
-                data = json.load(json_file)
-                await message.channel.send(data["a"])
-                json_file.close()
+            pollData = loadJsonData("json_poll.json")
+            await Mesage(message, pollData["a"])
+            #with open('json_poll.json') as json_file:
+                #data = json.load(json_file)
+                #await message.channel.send(data["a"])
+                #json_file.close()
 
     if message.content.startswith('$poll'):
         not0the0flash = await client.fetch_user(IDnot0the0flash)
         Gu = await client.fetch_user(IDguristic)
         if message.author == not0the0flash or Gu:
-            with open('json_poll.json') as json_file:
-                data = json.load(json_file)
-                x = discord.Embed(title=data["title"],color=0xFF5733)
-                x.set_thumbnail(url=linkGREGTECHLOGO)
-                x.add_field(name="Option A: ", value=data["a"], inline=True)
-                x.add_field(name="Option B: ", value=data["b"], inline=True)
-                mes = await message.channel.send(embed = x)
-                await addABReactions(mes)
-                json_file.close()
+            
+            pollData = loadJsonData("json_poll.json")
+            
+            x = discord.Embed(title=pollData["title"],color=0xFF5733)
+            x.set_thumbnail(url=linkGREGTECHLOGO)
+            x.add_field(name="Option A: ", value=pollData["a"], inline=True)
+            x.add_field(name="Option B: ", value=pollData["b"], inline=True)
+            mes = await message.channel.send(embed = x)
+            await addABReactions(mes)
+            
+            #with open('json_poll.json') as json_file:
+                #data = json.load(json_file)
+                #x = discord.Embed(title=data["title"],color=0xFF5733)
+                #x.set_thumbnail(url=linkGREGTECHLOGO)
+                #x.add_field(name="Option A: ", value=data["a"], inline=True)
+                #x.add_field(name="Option B: ", value=data["b"], inline=True)
+                #mes = await message.channel.send(embed = x)
+                #await addABReactions(mes)
+                #json_file.close()
 
     if message.content.startswith('$link'):
         await Mesage(message, linkREPO)
@@ -151,11 +220,14 @@ async def on_message(message):
             await Mesage(message,"No")
                      
     if message.content.startswith('$status'):
-        versionj = open("version.json", "r")
-        versionr = versionj.read()
-        version = json.loads(versionr)
-        versionj.close()
-        print(version)
+        #versionj = open("version.json", "r")
+        #versionr = versionj.read()
+        #version = json.loads(versionr)
+        #versionj.close()
+        
+        version = loadJsonData("version.json")
+        
+        #print(version)
         await message.channel.send(version["version"])
         
     if message.content.startswith('$newPoll '):
@@ -165,30 +237,37 @@ async def on_message(message):
             if len(result)>6:
                 lists = result.split(",")
                 if len(lists)==3:
-                    dicti = {"title":lists[0],"a":lists[1],"b":lists[2]}
-                    saveDict(dicti)
+                    newPoll = {"title":lists[0],"a":lists[1],"b":lists[2]}
+                    dumpJsonData("json_poll.json",newPoll)
+                    #saveDict(newPoll)
                     await message.channel.send("Poll Created Successfully!")
                     
     if message.content.startswith("$setStatus "):
         text = message.content
         if len(text)>10:
             result = find_between(text[10:],"(",")")
-            updateVersion(result)
+            #updateVersion(result)
+            versionData = {"version": result}
+            dumpJsonData("version.json", versionData)
             await message.channel.send("Status Set to: " + result)
             
     if message.content.startswith('$help'):
-        x = discord.Embed(title="Command Display",color=0xFF5733)
-        x.set_thumbnail(url=linkGREGTECHLOGO)
-        x.add_field(name="$hello", value=commandHelp["commands"]["hello"], inline=False)
-        x.add_field(name="$gtnh", value=commandHelp["commands"]["gtnh"], inline=False)
-        x.add_field(name="$poll", value=commandHelp["commands"]["poll"], inline=False)
-        x.add_field(name="$link", value=commandHelp["commands"]["link"], inline=False)
-        x.add_field(name="$newPoll", value=commandHelp["commands"]["newPoll"], inline=False)
-        x.add_field(name="$flip", value=commandHelp["commands"]["flip"], inline=False)
-        x.add_field(name="$status", value=commandHelp["commands"]["status"], inline=False)
-        x.add_field(name="$setStatus", value=commandHelp["commands"]["setStatus"],inline=False)
-        x.add_field(name="$help", value=commandHelp["commands"]["help"], inline=False)
-        mes = await message.channel.send(embed = x)
+        
+        #x = discord.Embed(title="Command Display",color=0xFF5733)
+        #x.set_thumbnail(url=linkGREGTECHLOGO)
+        #x.add_field(name="$hello", value=commandHelp["commands"]["hello"], inline=False)
+        #x.add_field(name="$gtnh", value=commandHelp["commands"]["gtnh"], inline=False)
+        #x.add_field(name="$poll", value=commandHelp["commands"]["poll"], inline=False)
+        #x.add_field(name="$link", value=commandHelp["commands"]["link"], inline=False)
+        #x.add_field(name="$newPoll", value=commandHelp["commands"]["newPoll"], inline=False)
+        #x.add_field(name="$flip", value=commandHelp["commands"]["flip"], inline=False)
+        #x.add_field(name="$status", value=commandHelp["commands"]["status"], inline=False)
+        #x.add_field(name="$setStatus", value=commandHelp["commands"]["setStatus"],inline=False)
+        #x.add_field(name="$help", value=commandHelp["commands"]["help"], inline=False)
+        
+        helpEmbed = loadHelpEmbed()
+        
+        mes = await message.channel.send(embed = helpEmbed)
 
 #Run the client
 client.run(botToken)
